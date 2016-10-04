8be9f53f45decca8658987df29a986529256e3c8
Mike Harder <mharder@microsoft.com>
2016-10-04 16:36:17 -0700

    Add benchmark using ASP.NET Core

--- NetCoreSyncPerf/Program.cs
+++ NetCoreSyncPerf/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace NetCoreSyncPerf
@@ -8,8 +9,12 @@ namespace NetCoreSyncPerf
     {
         private static readonly int _defaultThreads = Environment.ProcessorCount;
         private static readonly TimeSpan _defaultDuration = TimeSpan.FromSeconds(10);
-        private static readonly SyncPrimitive _defaultSyncPrimitive = SyncPrimitive.Lock;
+        private static readonly Sync _defaultSync = Sync.Lock;
         private static readonly Workload _defaultWorkload = Workload.Read;
+
+        // On a 12-core server, 7 iterations results in approximately the same RPS as PlaintextSyncPerf(sync=nolock, threads=256)
+        private static readonly int _defaultWorkOutsideLock = 7;
+        private static readonly int _defaultWorkInsideLock = 1;
 
         private static long _locksTaken = 0;
 
@@ -22,48 +27,68 @@ namespace NetCoreSyncPerf
 
             var threads = int.Parse(config["threads"] ?? _defaultThreads.ToString());
             var duration = TimeSpan.FromSeconds(int.Parse(config["duration"] ?? _defaultDuration.TotalSeconds.ToString()));
-            var syncPrimitive = (SyncPrimitive)Enum.Parse(typeof(SyncPrimitive),
-                config["syncPrimitive"] ?? _defaultSyncPrimitive.ToString(), ignoreCase: true);
+            var syncPrimitive = (Sync)Enum.Parse(typeof(Sync),
+                config["sync"] ?? _defaultSync.ToString(), ignoreCase: true);
             var workload = (Workload)Enum.Parse(typeof(Workload),
                 config["workload"] ?? _defaultWorkload.ToString(), ignoreCase: true);
+
+            var workOutsideLock = int.Parse(config["workOutsideLock"] ?? _defaultWorkOutsideLock.ToString());
+            var workInsideLock = int.Parse(config["workInsideLock"] ?? _defaultWorkInsideLock.ToString());
 
             Console.WriteLine($"Duration: {duration}");
             Console.WriteLine($"Threads: {threads}");
             Console.WriteLine($"SyncPrimitive: {syncPrimitive}");
             Console.WriteLine($"Workload: {workload}");
+            Console.WriteLine($"Work Outside Lock: {workOutsideLock}");
+            Console.WriteLine($"Work Inside Lock: {workInsideLock}");
+            Console.WriteLine();
 
-            CancellationToken cancellationToken;
+            var sw = new Stopwatch();
 
             var threadObjects = new Thread[threads];
             for (var i = 0; i < threads; i++)
             {
                 threadObjects[i] = new Thread(() =>
                 {
-                    if (syncPrimitive == SyncPrimitive.Lock)
+                    if (syncPrimitive == Sync.NoLock)
                     {
-                        while (!cancellationToken.IsCancellationRequested)
+                        while (sw.Elapsed < duration)
                         {
-                            lock (_lock)
-                            {
-                            }
+                            DoWork(workInsideLock);
+                            DoWork(workOutsideLock);
 
                             Interlocked.Increment(ref _locksTaken);
                         }
                     }
-                    else if (syncPrimitive == SyncPrimitive.ReaderWriterLockSlim)
+                    else if (syncPrimitive == Sync.Lock)
+                    {
+                        while (sw.Elapsed < duration)
+                        {
+                            lock (_lock)
+                            {
+                                DoWork(workInsideLock);
+                            }
+                            DoWork(workOutsideLock);
+
+                            Interlocked.Increment(ref _locksTaken);
+                        }
+                    }
+                    else if (syncPrimitive == Sync.Rwls)
                     {
                         if (workload == Workload.Read)
                         {
-                            while (!cancellationToken.IsCancellationRequested)
+                            while (sw.Elapsed < duration)
                             {
                                 _readerWriterLockSlim.EnterReadLock();
                                 try
                                 {
+                                    DoWork(workInsideLock);
                                 }
                                 finally
                                 {
                                     _readerWriterLockSlim.ExitReadLock();
                                 }
+                                DoWork(workOutsideLock);
 
                                 Interlocked.Increment(ref _locksTaken);
                             }
@@ -72,8 +97,7 @@ namespace NetCoreSyncPerf
                 });
             }
 
-            cancellationToken = new CancellationTokenSource(duration).Token;
-
+            sw.Start();
             for (var i = 0; i < threads; i++)
             {
                 threadObjects[i].Start();
@@ -83,18 +107,29 @@ namespace NetCoreSyncPerf
             {
                 threadObjects[i].Join();
             }
+            sw.Stop();
 
+            Console.WriteLine($"Actual Duration: {sw.Elapsed}");
             Console.WriteLine($"Total Locks Taken: {_locksTaken.ToString("N0")}");
 
-            var locksTakenPerSecond = (int)Math.Round(_locksTaken / duration.TotalSeconds, 0);
+            var locksTakenPerSecond = (int)Math.Round(_locksTaken / sw.Elapsed.TotalSeconds, 0);
             Console.WriteLine($"Locks Taken per Second: {locksTakenPerSecond.ToString("N0")}");
+        }
+
+        private static void DoWork(int iterations)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                DateTime.Now.ToString();
+            }
         }
     }
 
-    public enum SyncPrimitive
+    public enum Sync
     {
+        NoLock,
         Lock,
-        ReaderWriterLockSlim
+        Rwls,
     }
 
     public enum Workload
---
+++ PlaintextSyncPerf/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+
+namespace PlaintextSyncPerf
+{
+    public class Program
+    {
+        private static readonly int _defaultWorkInsideLock = 1;
+
+        private static readonly byte[] _noLockPayload = Encoding.UTF8.GetBytes("Hello,noLock!");
+        private static readonly byte[] _lockPayload = Encoding.UTF8.GetBytes("Hello,  lock!");
+        private static readonly byte[] _rwlsPayload = Encoding.UTF8.GetBytes("Hello,  rwls!");
+
+        private static readonly PathString _noLockPath = new PathString("/nolock");
+        private static readonly PathString _lockPath = new PathString("/lock");
+        private static readonly PathString _rwlsPath = new PathString("/rwls");
+
+        private static readonly object _lock = new object();
+        private static readonly ReaderWriterLockSlim _rwls = new ReaderWriterLockSlim();
+
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+
+            var workInsideLock = int.Parse(config["workInsideLock"] ?? _defaultWorkInsideLock.ToString());
+
+            Console.WriteLine($"Work Inside Lock: {workInsideLock}");
+            Console.WriteLine();
+
+            new WebHostBuilder()
+                .UseUrls("http://+:5000")
+                .UseKestrel()
+                .Configure(app =>
+                {
+                    app.Run(async context =>
+                    {
+                        context.Response.StatusCode = 200;
+                        context.Response.ContentType = "text/plain";
+                        context.Response.Headers["Content-Length"] = "13";
+
+                        byte[] payload;
+                        if (context.Request.Path.StartsWithSegments(_noLockPath, StringComparison.Ordinal))
+                        {
+                            DoWork(workInsideLock);
+                            payload = _noLockPayload;
+                        }
+                        else if (context.Request.Path.StartsWithSegments(_lockPath, StringComparison.Ordinal))
+                        {
+                            lock (_lock)
+                            {
+                                DoWork(workInsideLock);
+                            }
+                            payload = _lockPayload;
+                        }
+                        else if (context.Request.Path.StartsWithSegments(_rwlsPath, StringComparison.Ordinal))
+                        {
+                            _rwls.EnterReadLock();
+                            try
+                            {
+                                DoWork(workInsideLock);
+                            }
+                            finally
+                            {
+                                _rwls.ExitReadLock();
+                            }
+                            payload = _rwlsPayload;
+                        }
+                        else
+                        {
+                            throw new InvalidOperationException();
+                        }
+
+                        await context.Response.Body.WriteAsync(payload, 0, payload.Length);
+                    });
+                })
+                .Build()
+                .Run();
+        }
+
+        private static void DoWork(int iterations)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                DateTime.Now.ToString();
+            }
+        }
+    }
+}
